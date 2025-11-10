a0e96554dae9353fa534fc847a955d0c101f9cf2
yowanmada123 <yowanmada@gmail.com>
2025-11-10 09:57:35 +0700

    add pr feature

---
+++ lib/data/repository/approval_pr_repository.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:vivakencanaapp/models/fdpi/approval_pr/approval_pr.dart';
+
+import '../../models/errors/custom_exception.dart';
+import '../data_providers/rest_api/approval/approval_pr_rest.dart';
+
+class ApprovalPRRepository {
+  final ApprovalPRRest approvalPRRest;
+
+  ApprovalPRRepository({required this.approvalPRRest});
+
+  Future<Either<CustomException, List<ApprovalPR>>> getPRList() async {
+    return approvalPRRest.getPRList();
+  }
+
+  Future<Either<CustomException, String>> approvalPR({
+    required String prId,
+    required String typeAprv,
+  }) async {
+    return approvalPRRest.approvalPR(prId: prId, typeAprv: typeAprv);
+  }
+
+  Future<Either<CustomException, String>> rejectPR({
+    required String prId,
+    required String typeAprv,
+  }) async {
+    return approvalPRRest.rejectPR(prId: prId, typeAprv: typeAprv);
+  }
+}
